76f23a5f8d85fe4062b226ba256853740b37a4cc
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-11-19 15:47:38 +0100

    Add the subject under test
    [#161745282]

--- spec/integration/timeout_spec.rb
+++ spec/integration/timeout_spec.rb
@@ -3,7 +3,7 @@ require_relative 'fake_bits_service'
 require 'net/http'
 require 'spec_helper'
 
-describe BitsService::Client do
+describe BitsService::Client, :integration_test do
     let(:fake_server){}
     let(:fake_endpoint){'localhost'}
     let(:fake_endpoint_port){9292}
@@ -22,15 +22,16 @@ describe BitsService::Client do
         }
     end
 
-    subject(:client) { BitsService::Client.new(bits_service_options: options, resource_type: resource_type, vcap_request_id: vcap_request_id, request_timeout_in_seconds_fast: 3) }
-    before do
+    subject(:client) { BitsService::Client.new(bits_service_options: options, resource_type: resource_type, vcap_request_id: vcap_request_id, request_timeout_in_seconds_fast: 1) }
+    before() do
         opts = {
-            :Port => 9292,
-            :Host => fake_endpoint
+            Port: 9292,
+            Host: fake_endpoint,
+            AccessLog: []
           }
         fakeServer = Thread.new do
             Rack::Handler::WEBrick.run(FakeBitsService, opts) do |server|
-        #   server.log_file = "example.log"
+                # server.log_file = "/dev/null"
             end
         end
         # WebMock.allow_net_connect!
@@ -49,23 +50,54 @@ describe BitsService::Client do
 
     context 'HTTP Blobstore requests with little or no payload' do
         it "returns early when delte times out" do
-            request = Net::HTTP.new(fake_endpoint ,fake_endpoint_port)
-            request.read_timeout = 2
-            request.open_timeout = 2
-            startTime = Time.now
-            puts "Request Setup: #{request.read_timeout}"
-            response = nil
-            begin
+            # request = Net::HTTP.new(fake_endpoint ,fake_endpoint_port)
+            # request.read_timeout = 2
+            # request.open_timeout = 2
+            # startTime = Time.now
+            # puts "Request Setup: #{request.read_timeout}"
+            # response = nil
+            # begin
+            #     response = request.delete "/timeout"
+            # rescue => exception
+            #     puts "Error: #{exception.inspect}"
+            # end
+            # endTime = Time.now
+            # puts("Duration for /timeout: #{startTime - endTime}")
+            # puts response.inspect
 
-                response = request.delete "/timeout"
-            rescue => exception
-                puts "Error: #{exception.inspect}"
-            end
-            endTime = Time.now
-            puts("Duration for /timeout: #{startTime - endTime}")
-            puts response.inspect
-
+            startTime=Time.now
+            expect{
+                subject.delete(:key)
+            }.to raise_error(Net::ReadTimeout)
+            endTime=Time.now
+            puts("Duration for /delete: #{startTime - endTime}")
+            (startTime - endTime).should be < 3
         end
+
+        #  it 'times out fast when exists? is called' do
+    #  request = stub_request(:head , private_resource_endpoint).
+    #              to_return(status: 204, body: lambda { |request| sleep 2; "" })
+
+    #  startTime = Time.now
+    #  subject.send("exists?", key)
+    #  endTime = Time.now
+
+    #  end
+
+    # it 'times out fast when delete_all' do
+    # request = stub_request("GET" , private_resource_endpoint).
+    #             to_return(status: 204, body: lambda { |request| sleep 2; "" })
+
+    # subject.send("", key)
+    # end
+
+    # it 'times out fast when delete_all_in_path' do
+    # request = stub_request("GET" , private_resource_endpoint).
+    #             to_return(status: 204, body: lambda { |request| sleep 2; "" })
+
+    # subject.send("", key)
+    # end
+
 
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'rack/test'
 require 'steno'
 
 RSpec.configure do |rspec_config|
+
   rspec_config.before :all, unit: true do
       # Webmock does inject into the net http conncetion and for this reason it is only added to the path if its used.
       require 'webmock/rspec'
