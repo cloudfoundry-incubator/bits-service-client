a03b6a4d20c346471ace83f433aad81a30c47e5b
Norman Sutorius <norman.sutorius@de.ibm.com>
2019-01-29 14:01:19 +0100

    Extend the signature generation
    [#135220281]

--- lib/util/signature_util.rb
+++ lib/util/signature_util.rb
@@ -2,10 +2,10 @@
 
 module BitsService
   module SignatureUtil
-    def sign_signature(resource_path, key_secret, key_id)
+    def sign_signature(method, resource_path, key_secret, key_id)
       expires = seconds_since_the_unix_epoch_with_offset(3600)
       "#{resource_path}?" \
-        "signature=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, key_secret, "#{expires}#{resource_path} #{key_secret}")}&" \
+        "signature=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, key_secret, "#{method.upcase} #{resource_path} #{key_secret} #{expires}")}&" \
         "expires=#{expires}&" \
         "AccessKeyId=#{key_id}"
     end
